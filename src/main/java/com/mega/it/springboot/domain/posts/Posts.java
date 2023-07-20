49e13ece4a2c3f188f123e888c3583dd36d3fcc6
kim <xodus0118@gmail.com>
2023-07-20 17:43:39 +0900

    Day2

---
+++ src/main/java/com/mega/it/springboot/domain/posts/Posts.java
@@ -0,0 +1,38 @@
+package com.mega.it.springboot.domain.posts;
+
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor // public Posts(){} 만듬 생성자
+public class Posts{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 게터세터 대신에 클래스안에 클래스 넣어줘서 보안강화 디자인패턴
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content=content;
+        this.author=author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content=content;
+    }
+}
---
+++ src/main/java/com/mega/it/springboot/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,26 @@
+package com.mega.it.springboot.web.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.PostUpdate;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+
+    private String title;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String title, String content){
+        this.title=title;
+        this.content=content;
+    }
+
+
+
+
+}
